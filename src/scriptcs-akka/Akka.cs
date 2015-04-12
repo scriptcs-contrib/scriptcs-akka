ba4d5e2487c8b83107f56a5c45750b2a1af74f66
Glenn Block <glenn.block@gmail.com>
2015-04-12 15:33:32 -0700

    Initial commit

---
+++ src/scriptcs-akka/Akka.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ScriptCs;
+using ScriptCs.Contracts;
+
+namespace ScriptCs.Akka
+{
+    public class Akka : ScriptPack<IScriptPackContext>
+    {
+        public override void Initialize (IScriptPackSession session)
+        {
+            session.ImportNamespace ("Akka");
+            session.ImportNamespace ("Akka.Actor");
+            session.ImportNamespace ("Akka.Configuration.Hocon");
+
+            base.Initialize (session);
+        }
+    }
+
+    public class AkkaContext : IScriptPackContext {
+    }
+
+        
+}
+
